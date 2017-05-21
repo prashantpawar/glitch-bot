d98e88aef244a46bfdeef493ee0758a8656f62bc
System <none>
2017-05-21 21:24:49 +0000

    🍇🕴 Updated with Glitch

--- crowdsale_info.js
+++ crowdsale_info.js
@@ -1,7 +1,11 @@
 var config= {
-  "ethereum": "0x960b236a07cf122663c4303350609a66a7b288c0",
-  "bitcoin": "1edfgadsfgretetretet4",
-  "postponementMessage": "Please note that Tezos crowdsale which was scheduled for May 22 has been postponed to a further date in June"
+  "channel": "C5ERPH14H",
+  "ethereumAddress": "0x960b236a07cf122663c4303350609a66a7b288c0",
+  "bitcoinAddress": "1edfgadsfgretetretet4",
+  "getPostponementMsg": _ => ("Please note that Tezos crowdsale which was scheduled for May 22 has been postponed to a further date in June"),
+  "getCorrectAddressMsg": user => (`:white_check_mark: Hello <@${user}>! it looks like you posted the correct crowdsale address\n`),
+  "getIncorrectAddressMsg": user => (`:warning: :no_entry_sign: :skull_and_crossbones: Hey <@${user}>, it looks like you posted an ethereum address, but this is not the right crowdsale address, the correct address is ${config.ethereumAddress}\n`),
+  "messageFrequency" : 25
 };
 
 module.exports = config;
--- server.js
+++ server.js
@@ -8,48 +8,55 @@ const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 const crowdsaleInfo = require('./crowdsale_info');
 
 const bot_token = process.env.SLACK_BOT_TOKEN || '';
-let count = 1;
-let self;
-
-const channel = "C5ERPH14H"; //could also be a channel, group, DM, or user ID (C1234), or a username (@don)
+let state = {
+  count : 1
+};
 
 const rtm = new RtmClient(bot_token);
 rtm.start();
 
-const handleRtmMsg = R.curry(function handleRtmMessage(rtm, source, message) {
-  let returnMsg;
-  
-  if(count % 25 == 0) {
-    source.onNext(count);
+const postponeMsgReducer = R.curry((config, state, rtm, source) => {
+  if(state.count % config.messageFrequency == 0) {
+    source.onNext(state.count);
   }
-  count++;
-  
-  if (/(0x)?[0-9a-f]{40}/i.test(message.text)) {
-  // check if it has the basic requirements of an address
-    if (message.text.includes(crowdsaleInfo.ethereum)) {
-      returnMsg = `:white_check_mark: Hello <@${message.user}>! it looks like you posted the correct crowdsale address\n`;
-    } else {
-      returnMsg =  `:warning: :no_entry_sign: :skull_and_crossbones: Hey <@${message.user}>, it looks like you posted an ethereum address, but this is not the right crowdsale address, the correct address is ${crowdsaleInfo.ethereum}\n`;
-    }
-    
-    if(message.user != self.id) {
-      rtm.sendMessage(returnMsg, message.channel);
-    }
-  }
-  console.log(message);
+  state.count++;
+  return state;
 });
 
-const broadcastPostponementMsg = R.curry(function broadcastPostponementMsg(rtm) {
-  rtm.sendMessage(crowdsaleInfo.postponementMessage, channel);
+const ethereumMsgReducer = R.curry((config, state, rtm, source, message) => {
+    let returnMsg;
+
+    if (/(0x)?[0-9a-f]{40}/i.test(message.text)) {
+      // check if it has the basic requirements of an address
+      if (message.text.includes(config.ethereumAddress)) {
+        returnMsg = config.getCorrectAddressMsg(message.user);
+      } else {
+        returnMsg = config.getIncorrectAddressMsg(message.user);
+      }
+
+      if(message.user !== state.self.id) {
+        rtm.sendMessage(returnMsg, message.channel);
+      }
+    }
+    return state;
+  });
+
+const handleRtmMsg = R.curry((config, state, rtm, source, message) => {  
+  state = postponeMsgReducer(config, state, rtm, source);
+  state = ethereumMsgReducer(config, state, rtm, source, message);
+});
+
+const broadcastPostponementMsg = R.curry(function broadcastPostponementMsg(rtm, config) {
+  rtm.sendMessage(config.getPostponementMsg(), config.channel);
 });
 
 const source = new Rx.Subject();
 
-const subscription = source.subscribe(_ => broadcastPostponementMsg(rtm));
+const subscription = source.subscribe(_ => broadcastPostponementMsg(rtm, crowdsaleInfo));
 
-rtm.on(RTM_EVENTS.MESSAGE, handleRtmMsg(rtm, source));
+rtm.on(RTM_EVENTS.MESSAGE, handleRtmMsg(crowdsaleInfo, state, rtm, source));
 
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
-  self = rtmStartData.self;
-  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
+  state.self = rtmStartData.self;
+  console.log("Chatbot Initialized");
 });
