c65d9e7d95b997ca6242411aaba33fa8eeee57f3
System <none>
2017-05-21 18:36:27 +0000

    😎🕴 Updated with Glitch

--- crowdsale_info.js
+++ crowdsale_info.js
@@ -1,6 +1,7 @@
 var config= {
   "ethereum": "0x960b236a07cf122663c4303350609a66a7b288c0",
-  "bitcoin": "1edfgadsfgretetretet4"
+  "bitcoin": "1edfgadsfgretetretet4",
+  "postponementMessage": "Please note that Tezos crowdsale which was scheduled for May 22 has been postponed to a further date in June"
 };
 
 module.exports = config;
