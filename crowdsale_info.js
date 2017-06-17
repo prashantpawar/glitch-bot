9b6e3dd905cae64f218d4c3cdc27d9b96abaffe5
System <none>
2017-06-17 02:14:16 +0000

    🎸🤒 Updated with Glitch

--- crowdsale_info.js
+++ crowdsale_info.js
@@ -1,7 +1,16 @@
-var config= {
+const R = require('ramda');
+
+const config= {
   "getPostponementMsg": _ => ("Please note that Tezos fundraiser which was scheduled for May 22 has been postponed to a further date in June"),
   "getSuspiciousAddressMsg": _ => (':warning: the fundraiser hasn\'t started, do not be tricked into sending bitcoins or ether to phishers. Refer to https://www.tezos.com for official information.'),
-  "messageFrequency" : 25
+  "messageFrequency" : 25,
+  "getTopicByChannel": channel => R.propOr(
+    'Set a channel topic', 
+    channel, 
+    {
+      'C5ERPH14H': "What is the deal today?"
+    }
+  )
 };
 
 module.exports = config;
